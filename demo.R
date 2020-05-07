f8fe0bcc026c5e912abe1eb9024b7c1ab32ba53d
XingruiWang <XingrWang@163.com>
2020-05-07 19:19:49 +0800

    Update demo.R

--- demo.R
+++ demo.R
@@ -1,10 +1,22 @@
+score = read_csv("C:/Users/WXR/Desktop/2.csv", col_names = T)
+type = score$t
+score = score[2:11]
+score1=score[type==1,]
+score2=score[type==2,]
+score3=score[type==3,]
+
 ggradar2(score1,grid.max=6,group.point.size = 0,axis.line.colour = "grey", group.line.width = 0.5,
-        polygonfill.transparency = 0.05,
+        polygonfill.transparency = 0.03,
         plot.legend = FALSE,
         group.colours = '#FF6B6B',
         gridline.label = c("","",""))
 ggradar2(score2,grid.max=6,group.point.size = 0,axis.line.colour = "grey", group.line.width = 0.5,
-         polygonfill.transparency = 0.05,
+         polygonfill.transparency = 0.03,
          plot.legend = FALSE,
          group.colours = '#4ECDC4',
+         gridline.label = c("","",""))
+ggradar2(score3,grid.max=6,group.point.size = 0,axis.line.colour = "grey", group.line.width = 0.5,
+         polygonfill.transparency = 0.03,
+         plot.legend = FALSE,
+         group.colours = '#FFBA49',
          gridline.label = c("","",""))
