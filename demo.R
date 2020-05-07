b37a99ccd53895eabf984d040028583c4e56c6e6
XingruiWang <XingrWang@163.com>
2020-05-07 11:58:12 +0800

    Create demo.R

---
+++ demo.R
@@ -0,0 +1,5 @@
+ggradar(score,grid.max=6,group.point.size = 0,axis.line.colour = "grey", group.line.width = 0.5,group.line.alpha = 0.2,fill = T,
+        fill.alpha = 0.05,
+        legend.position = 'None',
+        group.colours = '#FF6B6B',
+        values.radar=c('','',''))
